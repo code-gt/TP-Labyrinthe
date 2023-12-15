5c070cc1775f3225641d2aeacbc8a7bcfb0c5c57
DevFoxie <hmerniz.dev@gmail.com>
2023-12-15 22:32:07 +0100

    game commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const maze = document.getElementById('maze');
+
+    // Créez une matrice représentant le labyrinthe
+    // const labyrinth = [
+        
+    // ];
+
+    // Affichez le labyrinthe dans la page HTML
+
+    // Position initiale du personnage
+    // let characterRow = 1;
+    // let characterCol = 1;
+
+    // Déplacez le personnage avec les touches du clavier
+
+    // Utilisez les touches fléchées (ArrowUp, ArrowDown, ArrowLeft, ArrowRight)
+    
+
+// Fonction pour déplacer le personnage
+// function moveCharacter(rowOffset, colOffset) {
+//     // Vérifiez si la nouvelle position est valide (pas un mur)
+ 
+//         // Déplacez le personnage
+       
+//         // Vérifiez si le joueur a atteint la sortie
+       
+//     }
+// }
+
+
+    // Fonction pour mettre à jour la classe CSS d'une cellule
+
+// function updateCell(row, col, newClass) {
+
+// }
